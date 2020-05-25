58126d44ed6f8eb76babeeb5b8698ded724ad5ea
Artur <arturrabbit3@gmail.com>
2020-05-26 01:42:04 +0300

    Добавьте файлы проекта.

---
+++ HomeWork15/HomeWork15.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+void PrintNums(int N, bool isTypingOdds)
+{
+	for (int i = 0; i <= N; i++)
+	{
+		if ((i % 2 != 0) == isTypingOdds)
+			std::cout << i << "\n";
+	}
+}
+
+int main()
+{
+    const int n = 20;
+	for (int i = 0; i <= n; i++)
+	{
+		if (i % 2 == 0)
+			std::cout << i << "\n";
+	}
+	std::cout << "first check" << "\n";
+	PrintNums(9, true);
+	std::cout << "second check" << "\n";
+	PrintNums(9, false);
+}
